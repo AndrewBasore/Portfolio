23e99655c562512274ab70367e7d21d94124cbac
Andrew Basore <basoreDevelopment@protonmail.com>
2018-05-24 18:54:40 -0400

    Implement Link and adding documentation

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -1,4 +1,13 @@
+/* 
+ *@author Andrew Basore
+ *
+ * intent: Make Navigational component 
+ * Sidebar component is major component of the 
+ * web app's layout. We use Link from react-router-dom to navigate our app.
+*/
+
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
 
 export default class Sidebar extends Component {
 
@@ -7,10 +16,10 @@ export default class Sidebar extends Component {
       <div className="sidebar">
         <div class="logo">Awesome<span>Portfolio</span></div>
         <nav>
-            <a href="/" class="nav-item">Home</a>
-            <a href="/about" class="nav-item">About</a>
-            <a href="/portfolio" class="nav-item active">Portfolio</a>
-            <a href="" class="nav-item">Contact</a>
+            <Link to="/" class="nav-item">Home</Link>
+            <Link to="/about" class="nav-item">About</Link>
+            <Link to="/portfolio" class="nav-item active">Portfolio</Link>
+            <Link to="/" class="nav-item">Contact</Link>
         </nav>
       </div>
     )
