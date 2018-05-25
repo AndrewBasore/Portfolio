5ee173c8c82841f40fa82a38796ece2b0ac60e7f
Andrew Basore <basoreDevelopment@protonmail.com>
2018-05-25 02:10:22 -0400

    minor style issues

--- src/js/components/Home/index.js
+++ src/js/components/Home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Sidebar extends Component {
+export default class Home extends Component {
 
   handleClick(){
     let firebaseRef = firebase.database().ref();
@@ -10,9 +10,11 @@ export default class Sidebar extends Component {
   render() {
     return (
       <div className="home">
-        <h1>Welcome!</h1>
+        <h1>Andrew Basore</h1>
         <img src="/dist/img/face-square" alt="Face of Andrew Basore"/>
-        <p>Aliquip voluptate voluptate eiusmod fugiat sunt enim pariatur in Lorem consectetur nisi sunt labore. Lorem occaecat magna ea aute consequat nulla veniam ex Lorem sunt nostrud qui. Ea aliquip proident in mollit incididunt non cupidatat ullamco veniam adipisicing. Aute id esse eu dolore laboris esse incididunt quis voluptate. Commodo cupidatat et sit laboris dolore occaecat aliquip velit pariatur dolor ut labore consectetur. Officia voluptate officia eiusmod consectetur cupidatat sint irure cillum ipsum exercitation pariatur tempor deserunt.</p>
+        <p>Highly self-motivated Fullstack developer with a strong analytical mind and a intruigue for exceptional UI design. With an understanding of front end web languages and frameworks including HTML5, CSS, NodeJS/ReactJS and Jquery. Problem solver who thrives in environments with ever changing technical and creative challenges.   In previous roles I have been hands on in design and development on the front end as well as designing server queries and back end architecture. I love working with ReactJS and dynamically driven data.
+
+Please enjoy checking out a few of the projects I have posted below. I am currently located in Tampa Bay Area Florida as a web developer so feel free to reach out through my email listed below! </p>
         
       </div>
     )
