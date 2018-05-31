1a5a3df467a86e294baed8bb334772cf4f000fdb
Andrew Basore <basoreDevelopment@protonmail.com>
2018-05-31 14:51:51 -0400

    Project components now display appropraite background image

--- src/js/components/Portfolio/Project.js
+++ src/js/components/Portfolio/Project.js
@@ -7,9 +7,16 @@ export default class Project extends Component {
   
     render() {
         let project = this.props.project;
+        let style = {
+            backgroundImage: project.backgroundImage
+        }
     return (
       
-        <div  className={`portfolio-item medium gray`}>{project.name}</div>
+        <div  className={`portfolio-item medium gray`} style={style}>
+            <div className={'over-lay'} >
+                {project.name}
+            </div>
+        </div>
       
     )
   }
