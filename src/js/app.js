1673f3077f88279e5b4419239159ba54aafeb58c
Andrew Basore <basoreDevelopment@protonmail.com>
2018-05-31 15:15:30 -0400

    Added icons for code and website links

--- src/js/app.js
+++ src/js/app.js
@@ -55,8 +55,8 @@ class Layout extends React.Component {
                     </p>    */}
                     <p id="author"><span>Coded by: </span>Andrew Basore </p>
                     <p id="social-media"><span>Social Media: </span>
-                        <a href="https://www.linkedin.com/in/andrew-basore-825235116/"><img src="https://kek.gg/i/7vwpT6.png" alt="Linkedin" /></a>
-                        <a href="https://github.com/AndrewBasore"><img className="github"src="https://kek.gg/i/7Cb8Rn.png" alt="Linkedin" /></a>
+                        <a href="https://www.linkedin.com/in/andrew-basore-825235116/"><img className="icon" src="https://kek.gg/i/7vwpT6.png" alt="Linkedin" /></a>
+                        <a href="https://github.com/AndrewBasore"><img className="github icon"src="https://kek.gg/i/7Cb8Rn.png" alt="Linkedin" /></a>
                     </p>
                     <p id="email"><span>Email: </span>basoreDev@protonmail.com</p>
                 </footer>
--- src/js/components/Portfolio/Project.js
+++ src/js/components/Portfolio/Project.js
@@ -13,7 +13,10 @@ export default class Project extends Component {
     return (
       
         <div  className={`portfolio-item medium`} style={style}>
-            <div className='links'></div>
+            <div className='links'>
+                <a href={project.href}><img  className="icon" src="/img/page-icon.png"/></a>
+                <a href={project.href}><img  className="icon" src="/img/code-icon.png"/></a>
+            </div>
             <div className='lay-over' >
                 <h3>{project.name}</h3>
             </div>
