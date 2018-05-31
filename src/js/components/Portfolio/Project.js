895c78f126c4a4295f21181442354251883d8652
Andrew Basore <basoreDevelopment@protonmail.com>
2018-05-31 16:09:40 -0400

    Portfolio-item moves overlay in on hover

--- src/js/components/Portfolio/Project.js
+++ src/js/components/Portfolio/Project.js
@@ -13,12 +13,12 @@ export default class Project extends Component {
     return (
       
         <div  className={`portfolio-item medium`} style={style}>
-            <div className='links'>
-                <a href={project.href}><img  className="icon" src="/img/page-icon.png"/></a>
-                <a href={project.href}><img  className="icon" src="/img/code-icon.png"/></a>
-            </div>
             <div className='lay-over' >
-                <h3>{project.name}</h3>
+                <div className='links'>
+                    <a href={project.href}><img  className="icon" src="/img/page-icon.png"/></a>
+                    <a href={project.href}><img  className="icon" src="/img/code-icon.png"/></a>
+                </div>
+                <h4>{project.name}</h4>
             </div>
         </div>
       
