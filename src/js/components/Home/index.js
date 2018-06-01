cb8581ac04b5b7b3456f2d7c872f4f026ee65d6a
Andrew Basore <basoreDevelopment@protonmail.com>
2018-06-01 15:51:37 -0400

    Added Contact component that contains contact information

--- src/js/components/Home/index.js
+++ src/js/components/Home/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Contact from './Contact.js';
 
 export default class Home extends Component {
 
@@ -13,6 +14,8 @@ export default class Home extends Component {
         <h1>Andrew Basore</h1>
         <img src="/img/face-square.jpg" id="face" alt="Face of Andrew Basore"/>
         <p>Welcome to my website! This portfolio of sample projects belongs to a Fullstack Web Developer. Built with popular front-end languages and frameworks like Javascript, React, Webpack, and CSSGrid this expresses the experience required to shine light on any brand on the web! Please feel free to contact me via E-mail or social media below.   </p>
+        <br />
+        <Contact />
       </div>
     )
   }
